2fa6fdac3143154aadc6fed67817ae436328d108
elee0068 <elee0068@student.monash.edu>
2024-04-22 20:07:36 +0800

    clients

--- client_main.py
+++ client_main.py
@@ -1,9 +1,11 @@
 from client_utils import *
 #"172.31.31.67"
 
-host = socket.gethostname()
+host = "54.165.235.72"
 port = 8888
 
 while True:
     client = initialise(host,port)
     sendData(client, get_current_gps_coordinates())
+    sendCoor(get_current_gps_coordinates())
+    sleep(10)
--- client_utils.py
+++ client_utils.py
@@ -3,6 +3,7 @@ import requests
 import socket
 from time import sleep
 import json
+import datetime
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 awsiot_endpoint = "a2cxs9th318s6p-ats.iot.ap-southeast-2.amazonaws.com" 
@@ -10,6 +11,13 @@ root_ca_path = "publish_cred/AmazonRootCA1.pem"
 private_key_path = "publish_cred/67afd4ef929b22d569c9f23c49a9b28615b8282a29077187214e5fc757569330-private.pem.key"
 certificate_path = "publish_cred/67afd4ef929b22d569c9f23c49a9b28615b8282a29077187214e5fc757569330-certificate.pem.crt"
 client_id = "Laptop"
+
+# Connect to AWS IoT Core
+myMQTTClient = AWSIoTMQTTClient(client_id)
+myMQTTClient.configureEndpoint(awsiot_endpoint, 8883)
+myMQTTClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
+
+myMQTTClient.connect()
 
 def get_current_gps_coordinates():
     g = geocoder.ip('me')#this function is used to find the current information using our IP Add
@@ -33,19 +41,13 @@ def initialise(host,port):
     client.connect((host,port))
     return client
 
-def sendData(data):
-    # Connect to AWS IoT Core
-    myMQTTClient = AWSIoTMQTTClient(client_id)
-    myMQTTClient.configureEndpoint(awsiot_endpoint, 8883)
-    myMQTTClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
+def sendData(client, data):
+    client.send(str(data).encode())
+    sleep(1)
+    client.close()
 
-    myMQTTClient.connect()
-
+def sendCoor(data):
     # Publish data to a topic
     topic = "lat/long"
+    data = {"id": client_id, "lat/long": data, "timestamp": str(datetime.datetime.now())}
     myMQTTClient.publish(topic, json.dumps(data), 0)
-
-    # Disconnect from AWS IoT Core
-    myMQTTClient.disconnect()
-
-sendData(get_current_gps_coordinates())
