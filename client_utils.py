2ce4c53704dd65cd63c1dafa041f6d26bf0da420
elee0068 <elee0068@student.monash.edu>
2024-04-22 20:15:37 +0800

    client sendCoor

--- client_utils.py
+++ client_utils.py
@@ -12,13 +12,6 @@ private_key_path = "publish_cred/67afd4ef929b22d569c9f23c49a9b28615b8282a2907718
 certificate_path = "publish_cred/67afd4ef929b22d569c9f23c49a9b28615b8282a29077187214e5fc757569330-certificate.pem.crt"
 client_id = "Laptop"
 
-# Connect to AWS IoT Core
-myMQTTClient = AWSIoTMQTTClient(client_id)
-myMQTTClient.configureEndpoint(awsiot_endpoint, 8883)
-myMQTTClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
-
-myMQTTClient.connect()
-
 def get_current_gps_coordinates():
     g = geocoder.ip('me')#this function is used to find the current information using our IP Add
     if g.latlng is not None: #g.latlng tells if the coordiates are found or not
@@ -47,7 +40,15 @@ def sendData(client, data):
     client.close()
 
 def sendCoor(data):
+    # Connect to AWS IoT Core
+    myMQTTClient = AWSIoTMQTTClient(client_id)
+    myMQTTClient.configureEndpoint(awsiot_endpoint, 8883)
+    myMQTTClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
+
+    myMQTTClient.connect()
     # Publish data to a topic
     topic = "lat/long"
     data = {"id": client_id, "lat/long": data, "timestamp": str(datetime.datetime.now())}
     myMQTTClient.publish(topic, json.dumps(data), 0)
+
+    myMQTTClient.disconnect()
