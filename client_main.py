e4988d2eb72a5c44de82309920d5c4dab8e1cfb8
elee0068 <elee0068@student.monash.edu>
2024-04-25 21:40:29 +0800

    documentation for client

--- client_main.py
+++ client_main.py
@@ -1,11 +1,16 @@
 from client_utils import *
-#"172.31.31.67"
 
-host = "54.165.235.72"
+#initialise host and port 
+host = "52.90.148.84"
 port = 8888
 
+#send location continuously
 while True:
+    #establish connection to server
     client = initialise(host,port)
+    #send gps coordinates to server
     sendData(client, get_current_gps_coordinates())
+    #send coordinates to iot topic
     sendCoor(get_current_gps_coordinates())
+    #buffer 10 seconds before rerunning the loop
     sleep(10)
--- client_utils.py
+++ client_utils.py
@@ -6,49 +6,50 @@ import json
 import datetime
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
+#AWS  credentials
 awsiot_endpoint = "a2cxs9th318s6p-ats.iot.ap-southeast-2.amazonaws.com" 
 root_ca_path = "publish_cred/AmazonRootCA1.pem"
 private_key_path = "publish_cred/67afd4ef929b22d569c9f23c49a9b28615b8282a29077187214e5fc757569330-private.pem.key"
 certificate_path = "publish_cred/67afd4ef929b22d569c9f23c49a9b28615b8282a29077187214e5fc757569330-certificate.pem.crt"
 client_id = "Laptop"
 
+#function to get gps coordinates
 def get_current_gps_coordinates():
-    g = geocoder.ip('me')#this function is used to find the current information using our IP Add
-    if g.latlng is not None: #g.latlng tells if the coordiates are found or not
+    #this function is used to find the current information using IP Address
+    g = geocoder.ip('me')
+    #g.latlng tells if the coordiates are found or not
+    if g.latlng is not None:
         return g.latlng
     else:
         return None
 
-def get_user_location():
-    try:
-        response = requests.get('https://ipinfo.io')
-        data = response.json()
-        print(data['loc'])
-        return data['loc']
-    except:
-        print("Error: Unable to detect your location.")
-        return None
-
+#function to establish connection to server
 def initialise(host,port):
     client = socket.socket()
     client.connect((host,port))
     return client
 
+#function to send data to server 
 def sendData(client, data):
+    #send encoded data to server
     client.send(str(data).encode())
     sleep(1)
+    #close connection to server
     client.close()
 
+#function to send data to aws iot topic
 def sendCoor(data):
-    # Connect to AWS IoT Core
+    #connect to AWS IoT Core
     myMQTTClient = AWSIoTMQTTClient(client_id)
     myMQTTClient.configureEndpoint(awsiot_endpoint, 8883)
     myMQTTClient.configureCredentials(root_ca_path, private_key_path, certificate_path)
 
     myMQTTClient.connect()
-    # Publish data to a topic
+
+    #publish data to topic
     topic = "lat/long"
     data = {"id": client_id, "lat/long": data, "timestamp": str(datetime.datetime.now())}
     myMQTTClient.publish(topic, json.dumps(data), 0)
 
+    #disconnect from IOT Core
     myMQTTClient.disconnect()
